b50dc9ba967e517fbe613339ffb10abbe4ae2647
lclc98 <liam.c14785@gmail.com>
2019-09-17 20:42:43 +1000

    Fixed CDN path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.BASE_URL,
   lintOnSave: true,
   devServer: {
     allowedHosts: (process.env.ALLOWED_HOSTS && process.env.ALLOWED_HOSTS.split(',')) || []
